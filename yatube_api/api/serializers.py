4707a5c342e61cd09b8542ec29c5c6b55501ef4c
Nikita Neupokoev <n.neupokoev154@yandex.ru>
2025-03-26 21:27:39 +0700

    API_final_project_review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,111 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели User.
+
+    Предоставляет информацию о:
+    - ID пользователя;
+    - Имени пользователя.
+    """
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    """Сериализатор для модели Post.
 
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
+    Предоставляет информацию о:
+    - ID поста;
+    - Тексте поста;
+    - Авторе (только для чтения);
+    - Изображении (опционально);
+    - Группе (опционально);
+    - Дате публикации.
+    """
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True,
+        slug_field='username'
     )
 
     class Meta:
-        fields = '__all__'
+        model = Post
+        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group.
+
+    Предоставляет информацию о:
+    - ID группы;
+    - Названии группы;
+    - Slug группы;
+    - Описании группы.
+    """
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comment.
+
+    Предоставляет информацию о комментарии, включая:
+    - ID;
+    - Автора (только для чтения);
+    - Post (ID поста, только для чтения);
+    - Текст комментария;
+    - Дату создания.
+
+    Примечания:
+    1) Поле `post` доступно только для чтения.
+    """
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
         model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Follow (подписки пользователей).
+
+    Notes:
+        1) 'user' заполняется автоматически текущим пользователем при создании.
+        2) Используется `SlugRelatedField` для отображения username вместо ID.
+    """
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            ),
+        ]
+
+    def validate_following(self, data):
+        if data == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,112 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer
+)
+from posts.models import Follow, Group, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Post.
+
+    Предоставляет CRUD-операции для постов.
+    Автоматически устанавливает автора поста при создании.
+
+    Notes:
+        1)  Автор поста устанавливается автоматически как текущий пользователь.
+        2)  Используется `LimitOffsetPagination` для постраничного вывода.
+        3)  Если в запросе указана группа, она связывается с постом.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            group=serializer.validated_data.get('group')
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Comment.
+
+    Предоставляет CRUD-операции для комментариев к посту.
+    Автоматически устанавливает автора и пост при создании комментария.
+
+    Notes:
+        1)  ID поста передается в URL как `post_id`.
+        2)  Автор комментария устанавливается автоматически как.
+    текущий пользователь.
+        3)  Пост, к которому относится комментарий, находится по.
+    `post_id` в URL.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        return get_object_or_404(
+            Post, pk=self.kwargs.get('post_id')
+        ).comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=get_object_or_404(
+                Post, pk=self.kwargs.get('post_id')
+            )
+        )
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet для модели Group.
+
+    Предоставляет доступ только для чтения к группам.
+
+    Notes:
+        1)  Используется `ReadOnlyModelViewSet`, так как группы не должны.
+    создаваться/редактироваться через API.
+        2)  Доступ к группам разрешен всем.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели Follow (подписки).
+
+    Предоставляет операции для подписки на пользователей.
+
+    Notes:
+        1)  Поддерживаются только операции получения списка подписок (GET) и.
+        создания подписки (POST).
+        2)  Доступна фильтрация по username пользователя, на которого.
+        подписываются.
+        3)  Поле 'user' (кто подписывается) устанавливается автоматически как.
+        текущий пользователь при создании.
+        4)  Для получения списка подписок используется обратная связь.
+        `follower` от модели `User`.
+    """
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('following__username',)
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
